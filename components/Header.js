8c14a838005391a2585585e7ddf452b4ad9faa64
Jacob Williams <wjacobb@byui.edu>
2022-09-12 19:17:46 -0600

    Small clean up

--- components/Header.js
+++ components/Header.js
@@ -34,24 +34,14 @@ function Header() {
                 <h1 className="font-bold text-xl">Daydream Car Company</h1>
                 <h2 className="hidden md:flex">&emsp;&emsp;Car rental you'll dream about</h2>
             </div>
-            {/* Middle */}
-            <div className='hidden relative lg:grid h-10 w-20 my-auto'>
-                <a href="https://www.youtube.com/watch?v=uZ57I2A3j5Y">
-                    <Image src="https://images.turo.com/media/vehicle/images/ABo9f8s3QLuBwkjCwE4PfQ.720x350.jpg"
-                        layout="fill"
-                        objectFit="contain" 
-                        objectPosition="left"
-                        className="cursor-pointer"
-                    />
-                </a>
-            </div>            
+          
             {/* Right */}
-            <div className="my-auto">
+            <div className="my-auto lg:col-span-2">
                 {/* Regular tabs */}
                 <div className='navbar-burger burger hidden lg:flex items-center justify-end text-black'>
                     {/* Header tabs */} 
                     <button onClick={home} className='hidden md:inline cursor-pointer hover:underline lg:px-2 rounded-md py-1'>Home</button>
-                    <button onClick={features} className='hidden md:inline cursor-pointer hover:underline lg:px-2 rounded-md py-1'>Features</button>                        
+                    {/* <button onClick={features} className='hidden md:inline cursor-pointer hover:underline lg:px-2 rounded-md py-1'>Features</button>                         */}
                     <button onClick={contact} className='hidden md:inline cursor-pointer hover:underline lg:px-2 rounded-md py-1'>Contact</button>
                     <button onClick={signIn} className='hidden md:inline cursor-pointer hover:underline lg:px-2 rounded-md py-1'>Sign In</button>                  
                 </div>
--- components/Insurance.js
+++ components/Insurance.js
@@ -3,16 +3,14 @@ import Image from 'next/image'
 function Insurance({ name, imgURL, price }) {
     return(
         <div>
-            <a href="https://www.youtube.com/watch?v=tAVnGvRwXew">
-                <section>
-                    <div className='flex items-center px-4'>
-                        <div className='relative h-10 w-10 mx-2 mb-2'>
-                            <Image alt='my-image' src={imgURL} layout='fill' className='rounded-lg'/>
-                        </div>
-                        <button className="mb-2 w-full inline-block px-6 py-2.5 bg-green-800 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-green-900 hover:shadow-lg active:bg-green-900 focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out hover:scale-105 active:scale-100">{name} ${price}</button>                
+            <section>
+                <div className='flex items-center px-4'>
+                    <div className='relative h-10 w-10 mx-2 mb-2'>
+                        <Image alt='my-image' src={imgURL} layout='fill' className='rounded-lg'/>
                     </div>
-                </section>    
-            </a>
+                    <button className="mb-2 w-full inline-block px-6 py-2.5 bg-green-800 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-green-900 hover:shadow-lg active:bg-green-900 focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out hover:scale-105 active:scale-100">{name} ${price}</button>                
+                </div>
+            </section>    
         </div>
     )
 }
